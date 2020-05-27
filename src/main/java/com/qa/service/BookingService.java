8a4a399001f21a7ed8367dda162e7ced94666790
Korbinian <kmr.ring@outlook.com>
2020-05-27 10:33:18 +0100

    second commit

---
+++ src/main/java/com/qa/service/BookingService.java
@@ -0,0 +1,57 @@
+package com.qa.service;
+
+import com.qa.domain.Bookings;
+import com.qa.exception.BookingNotFoundException;
+import com.qa.repo.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingService {
+    private final BookingRepository repo;
+
+    @Autowired
+    public BookingService(BookingRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Bookings> readAllBookings(){
+        return this.repo.findAll();
+    }
+
+    public Bookings createBooking(Bookings booking){
+        return this.repo.save(booking);
+    }
+
+    public Bookings findBookingById(Long id){
+        return this.repo.findById(id).orElseThrow(BookingNotFoundException::new);
+    }
+
+    public Bookings updateBooking(Long id, Bookings booking){
+        Bookings update = findBookingById(id);
+        update.setMovieId(booking.getMovieId());
+        update.setAdultNr(booking.getAdultNr());
+        update.setChildNr(booking.getChildNr());
+        update.setCustomerName(booking.getCustomerName());
+        update.setDateTime(booking.getDateTime());
+        update.setEmailAddress(booking.getEmailAddress());
+        update.setMovieName(booking.getMovieName());
+        update.setPhoneNumber(booking.getPhoneNumber());
+        update.setStudentNr(booking.getStudentNr());
+        update.setTotalPrice(booking.getTotalPrice());
+
+
+        return this.repo.save(update);
+    }
+
+    public boolean deleteBooking(Long id){
+        if(!this.repo.existsById(id)){
+            throw new BookingNotFoundException();
+        }
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
+    }
+
+}
