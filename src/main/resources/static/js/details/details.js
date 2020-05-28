f70f732c02c02d3159261fd9a2a212a3d325f8e3
Korbinian <kmr.ring@outlook.com>
2020-05-28 23:48:31 +0100

    attempt at making dates work

--- src/main/resources/static/js/details/details.js
+++ src/main/resources/static/js/details/details.js
@@ -1,5 +1,8 @@
 const params = new URLSearchParams(window.location.search)
 
+let dates = ["28/05/2020 14:30","28/05/2020 16:30","29/05/2020 14:30","28/05/2020 20:30","29/05/2020 18:30","27/05/2020 14:30"];
+
+
 function writeContent(apiID) {
     axios.get(`https://api.themoviedb.org/3/movie/${apiID}?api_key=e8787f4d45be4c1bcdb939f0d6113db5&language=en-US`).then(
         fill => {
@@ -57,11 +60,59 @@ function writeContent(apiID) {
     )
 }
 
+function dateSelect(dates) {
+    let templist = [];
+    let bigParent = document.getElementById("mCSB_1_container");
+
+    for (i=0;i<dates.length;){
+        let day = document.createElement("div")
+        day.className="accordion__card"
+        let insert ="";
+        templist.push(dates[0]);
+        dates.splice(0,1);
+
+        for(let j=0; j<dates.length;j++){
+            if(dates[j].split(" ")[0]===templist[0].split(" ")[0]){
+                templist.push(dates[j]);
+                dates.splice(j,1)
+            }
+        }
+
+        for(let j=0; j<templist.length;j++){
+            insert+=`<tbody><tr><th>${templist[j].split(" ")[1]}</th></tr></tbody>`
+        }
+
+        day.innerHTML= `<div class="card-header" id="headingOne">
+      <button type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <span>Season: 1</span>
+        <span>${templist[0].split(" ")[0]}</span>
+      </button>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        <table class="accordion__list">`+
+
+            insert+
+
+            `</table>
+      </div>
+    </div>`;
+
+        bigParent.appendChild(day);
+
+
+        console.log(templist);
+        templist = [];
+    }
+}
+
 axios.get(`http://localhost:8080/movie/get/${params.get('id')}`).then(
     write => {
         console.log(write.data);
         writeContent(write.data.apiID);
-        videoSource(write.data.apiID)
+        videoSource(write.data.apiID);
+        dateSelect(dates);
     }
 )
 
