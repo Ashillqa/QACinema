33b593bd7a7fd653afc589e5057120e93f48aebf
Korbinian <kmr.ring@outlook.com>
2020-05-28 14:05:23 +0100

    added init data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,20 @@
-insert into movie (id, apiid, status) values (20,13241,'showing');
+insert into movie (id, apiid, status) values (118,22428,'showing');
+insert into movie (id, apiid, status) values (119,429422,'showing');
+insert into movie (id, apiid, status) values (120,110415,'showing');
+insert into movie (id, apiid, status) values (121,16257,'showing');
+insert into movie (id, apiid, status) values (122,512200,'showing');
+insert into movie (id, apiid, status) values (123,515001,'showing');
+
+insert into movie (id, apiid, status) values (124,419704,'featured');
+insert into movie (id, apiid, status) values (125,338762,'featured');
+insert into movie (id, apiid, status) values (126,495764,'featured');
+insert into movie (id, apiid, status) values (127,454626,'featured');
+insert into movie (id, apiid, status) values (128,38700,'featured');
+insert into movie (id, apiid, status) values (129,475557,'featured');
+
+insert into movie (id, apiid, status) values (130,400160,'upcoming');
+insert into movie (id, apiid, status) values (131,514847,'upcoming');
+insert into movie (id, apiid, status) values (132,556678,'upcoming');
+insert into movie (id, apiid, status) values (133,696007,'upcoming');
+insert into movie (id, apiid, status) values (134,524047,'upcoming');
+insert into movie (id, apiid, status) values (135,572751,'upcoming');
