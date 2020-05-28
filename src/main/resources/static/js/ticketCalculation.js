6166d2f2608b6bf7453a4f89a562cd8d2c983848
David <temiloluwaw@gmail.com>
2020-05-28 18:42:34 +0100

    was able to calculate the total price based on the amounts selected in the drop-downs for the adult, child and student

--- src/main/resources/static/js/ticketCalculation.js
+++ src/main/resources/static/js/ticketCalculation.js
@@ -1,25 +1,37 @@
+let adultNumber = 0;
+    let childNumber = 0;
+    let studentNumber = 0;
+
 function updateTotal(type){
+
     var id = "totalPrice";
 
     console.log(type);
+    
 
     if(type === 'adult'){
+        let adult = document.getElementById(type);
+        adultNumber = adult.options[adult.selectedIndex].value;
     }
     else if(type === 'child'){
-
+        let child = document.getElementById(type);
+        childNumber = child.options[child.selectedIndex].value;
+    }
+    else if(type === 'student'){
+        let student = document.getElementById(type);
+        studentNumber = student.options[student.selectedIndex].value;
     }
 
-    let adult = document.getElementById(type);
+    let total = (adultNumber * 10) + (childNumber * 5) + (studentNumber * 7.5);
 
-    let number = adult.options[adult.selectedIndex].value;
 
-    console.log(adultChoice);
+    console.log(total);
 
     document.getElementById(id).className = "section__title";
 
-    let total;
+    
 
-    document.getElementById(id).textContent = number;
+    document.getElementById(id).textContent = "£"+total;
     
 }
 
