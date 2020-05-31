646d0e81cdd1045b50242280e80957a66e1c9787
Ashillqa <ashillpathak@gmail.com>
2020-05-31 14:12:20 +0100

    somehow was still in the unstaged files list

---
+++ src/main/resources/static/js/filmInsert.js
@@ -0,0 +1,39 @@
+let list = [];
+let ids = [];
+
+axios.get(`http://localhost:8080/movie/getAll`).then(
+        data => {
+            for(let i of data.data){
+                if (i.status==="upcoming"){
+                    continue;
+                }
+                list.push(i.apiID);
+                ids.push(i.id);
+            }
+            someFunc(list);
+        }
+    )
+    
+    
+    function someFunc(list){
+	
+	let movieDivision = document.getElementById('movieTitle');
+	for(let i=0;i<list.length;i++){
+        let movieOpt = document.createElement('option');
+        axios.get(`https://api.themoviedb.org/3/movie/${list[i]}?api_key=e8787f4d45be4c1bcdb939f0d6113db5&language=en-UK`).then(
+            append => {
+                movieOpt.innerHTML = 
+                 `${append.data.title}`+
+                  
+                 '</option>';
+                            
+                movieDivision.appendChild(movieOpt);
+            }
+        )
+    }
+}
+
+	
+	
+	
+	
