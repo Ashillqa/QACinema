2cd81d4478761ec9a0a36f436b3557a70ec1a3c7
David <temiloluwaw@gmail.com>
2020-06-02 15:59:58 +0100

    included the thank you for your feedback pop up complete

--- src/main/resources/static/js/emailSending.js
+++ src/main/resources/static/js/emailSending.js
@@ -10,7 +10,9 @@ function sendEmail() {
 
     console.log(info);
     axios.post(`http://${window.location.href.toString().split("/")[2]}/sendEmail`, JSON.parse(info)).then(res =>  {
-        alert("Thank you for your feedback");
+        alert("Thank you for your feedback!\n_______________________________\n" +
+            "Your Email has now been successfully sent!\n_______________________________\n" +
+        "We will get back to you if we need to via the email address that you have specified");
             console.log(res);
             // location.replace("paymentComplete.html");
         });
