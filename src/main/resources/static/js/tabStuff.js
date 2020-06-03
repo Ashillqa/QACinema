893974876b78aebef4b23e4f28000c35a4557318
David <temiloluwaw@gmail.com>
2020-06-03 13:07:11 +0100

    fixed up the home page by removing the carousel and just having a still image

---
+++ src/main/resources/static/js/tabStuff.js
@@ -0,0 +1,17 @@
+document.getElementById("1-tab").click();
+
+function openTab(evt, tabsName) {
+               var i, tabcontent, tablinks;
+               tabcontent = document.getElementsByClassName("tabcontent");
+               for (i = 0; i < tabcontent.length; i++) {
+                  tabcontent[i].style.display = "none";
+               }
+               tablinks = document.getElementsByClassName("tablinks");
+               for (i = 0; i < tablinks.length; i++) {
+                  tablinks[i].className = tablinks[i].className.replace(" active", "");
+               }
+               document.getElementById(tabsName).style.display = "block";
+               evt.currentTarget.className += " active";
+            }
+            // Get the element with id="defaultOpen" and click on it
+            document.getElementById("defaultOpen").click();
